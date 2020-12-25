0e15373c2bb20678ef83ec023c4fc23b7f5bac22
Caleb Anderson <caleb@Calebs-MacBook-Air.local>
2020-12-25 17:07:38 -0600

    I guess it works

--- main.cpp
+++ main.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
-//#include <cmath>
 using namespace std;
 
-string cypher(int);
+string cipherAlpha(int, string);
+string cipherMessage(string, string, string);
 
 int main() {
 
     int key = 0;
     char discard;
-    string message;
-    string alphaCypher;
+    string message, cypherAlpha, cipherText, alpha = "abcdefghijklmnopqrstuvwxyz";
 
-    cout << "Ceasar Cypher Shift (key), (1-25): ";
-    cin  >> key;
-    cin.get(discard);
+    cout << "Ceasar Shift Cipher key? (0-25): ";
+    cin >> key;
+    if (key > 25) {
+        while(key > 25 || key < 0) {
+            cout << "Error! Enter an integer between 0 and 25: ";
+            cin >> key;
+        }
+    }
+    cin.get(discard); // discard \n
 
     cout << "Message: ";
     getline(cin, message);
 
-    cout << "Your message in plaintext: " << message << endl;
-    cout << "Your Ceasar Cypher message: " << cypher(key) << endl;
+    cypherAlpha = cipherAlpha(key, alpha); // shift alphabet [key] letters to the left
+    cipherText = cipherMessage(cypherAlpha, alpha, message); // encrypt the message with the cipher alphabet
+
+    cout << "Plaintext message: " << message << endl;
+    cout << "Normal alphabet: " << alpha << endl;
+    cout << "Cipher alphabet: " << cypherAlpha << endl;
+    cout << "Ceasar Cipher message: " << cipherText << endl;
 
     return 0;
 }
 
-string cypher(int key) {
-//    string cyphertext;
-    string alpha = "abcdefghijklmnopqrstuvwxyz";
-    string cypherAlpha = "abcdefghijklmnopqrstuvwxyz";
-
+string cipherAlpha(int key, string alpha) {
+    string cypherAlpha = alpha; // cypherAlpha same size as alpha
+    int p = 0; // wrap around variable
     for (int i = 0; i < alpha.size(); i++) {
         if (i + key >= alpha.size()) {
-            cypherAlpha[i] = alpha[--key];
+            cypherAlpha[i] = alpha[p];
+            p++;
         } else {
             cypherAlpha[i] = alpha[i + key];
         }
     }
     return cypherAlpha;
+}
+
+string cipherMessage(string cipherAlpha, string alpha, string message) {
+    string cipherText = message; // cipherText same size as message
+    for(int i = 0; i < alpha.size(); i++) {
+        for(int j = 0; j < message.size(); j++) {
+            if(alpha[i] == message[j])
+                cipherText[j] = cipherAlpha[i];
+        }
+    }
+    return cipherText;
 }
